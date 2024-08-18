f7c7e18c3a995ae1fd673439969fb645e96c6f9a
the-jey <jey.pley@gmail.com>
2024-08-18 14:19:32 +0200

    get products endpoint

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/the-jey/littleecomgoapi/service/product"
 	"github.com/the-jey/littleecomgoapi/service/user"
 )
 
@@ -28,6 +29,10 @@ func (s *APIServer) Run() error {
 	userStore := user.NewStore(s.db)
 	userHandler := user.NewHandler(userStore)
 	userHandler.RegisterRoutes(subrouter)
+
+	productStore := product.NewStore(s.db)
+	productHandler := product.NewHandler(productStore)
+	productHandler.RegisterRoutes(subrouter)
 
 	log.Println("Listening on", s.addr)
 
---
+++ service/product/routes.go
@@ -0,0 +1,31 @@
+package product
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/the-jey/littleecomgoapi/types"
+	"github.com/the-jey/littleecomgoapi/utils"
+)
+
+type Handler struct {
+	store types.ProductStore
+}
+
+func NewHandler(store types.ProductStore) *Handler {
+	return &Handler{store: store}
+}
+
+func (h *Handler) RegisterRoutes(router *mux.Router) {
+	router.HandleFunc("/products", h.handleGetProducts).Methods(http.MethodGet)
+}
+
+func (h *Handler) handleGetProducts(w http.ResponseWriter, r *http.Request) {
+	ps, err := h.store.GetProducts()
+	if err != nil {
+		utils.WriteError(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	utils.WriteJSON(w, http.StatusOK, ps)
+}
